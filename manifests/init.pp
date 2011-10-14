8660771a964b04287086842dc50072eaa4fa124d
Alfred Chan <alfredwchan@gmail.com>
2011-10-14 16:54:59 -0400

    Set JAVA_HOME environment variable.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,11 @@ class open-java {
   package { "openjdk-6-jdk":
     ensure => installed,
   }
+  
+  exec { "java-home":
+    command => "bash -c 'echo JAVA_HOME=/usr/lib/jvm/java-6-openjdk/ >> /etc/environment'",
+    unless => "grep 'JAVA_HOME=' /etc/environment",
+    path => ["/bin", "/usr/bin"],
+    require => Package["openjdk-6-jdk"],
+  }
 }
