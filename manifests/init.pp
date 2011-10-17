a256d92bb76ad17564af3a43f610d502743b57e0
Alfred Chan <alfredwchan@gmail.com>
2011-10-17 04:08:29 -0400

    Don't source bashrc after modifying.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,7 @@ class open-java {
   }
   
   exec { "java-home":
-    command => "bash -c \"sed -i '1i JAVA_HOME=/usr/lib/jvm/java-6-openjdk/' /etc/bash.bashrc\" && . /etc/bash.bashrc",
+    command => "sed -i '1i JAVA_HOME=/usr/lib/jvm/java-6-openjdk/' /etc/bash.bashrc",
     unless => "grep 'JAVA_HOME=' /etc/bash.bashrc",
     path => ["/bin", "/usr/bin"],
     require => Package["openjdk-6-jdk"],
